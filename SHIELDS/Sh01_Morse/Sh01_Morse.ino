38b3e8f2ae2d5690ae0536128418d0598002b893
tcnct <tcnct@localhost.localdomain>
2020-09-03 02:48:33 -0300

    Shield continuidade

--- SHIELDS/Sh01_Morse/Sh01_Morse.ino
+++ SHIELDS/Sh01_Morse/Sh01_Morse.ino
@@ -1,25 +1,8 @@
 #include "morse.h"
 
-int falante = 3;
-
 void setup()
 {
-
-  String str = "sos";
-
-  // Length (with one extra character for the null terminator)
-  int str_len = str.length() + 1;
-
-  // Prepare the character array (the buffer)
-  char char_array[str_len];
-
-  // Copy it over
-  str.toCharArray(char_array, str_len);
-
-  for (int i = 0; i < str.length(); i++)
-  {
-    teclar(str[i]);
-  }
+  teclarFrase("sos");
 }
 
 void loop()
